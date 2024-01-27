0336c135ec1c4fce33306644c13dae04b90f7fa6
magnus <11580453+rowmagnon@users.noreply.github.com>
2024-01-27 17:43:58 +1100

    Update metaset.js

--- src/js/metaset.js
+++ src/js/metaset.js
@@ -1,1 +1,14 @@
-
+		  // Dynamically set meta elements in head
+	    document.addEventListener('DOMContentLoaded', function() {
+	      var hostname = window.location.hostname;
+	
+	      document.title = 'Exclusive Domain Sale - ' + hostname;
+	      document.querySelector('meta[name="description"]').setAttribute('content', 'Secure the exclusive domain ' + hostname + ', a unique opportunity to elevate your online presence.');
+	      document.querySelector('link[rel="canonical"]').setAttribute('href', 'https://' + hostname);
+	      document.querySelector('meta[property="og:title"]').setAttribute('content', 'Exclusive Domain Sale - ' + hostname);
+	      document.querySelector('meta[property="og:description"]').setAttribute('content', 'Secure the exclusive domain ' + hostname + ', a unique opportunity to elevate your online presence.');
+	      document.querySelector('meta[property="og:url"]').setAttribute('content', 'https://' + hostname);
+	      // Set the og:image and twitter:image src if you have a dynamic way to generate or fetch these images
+	      // document.querySelector('meta[property="og:image"]').setAttribute('content', 'https://' + hostname + '/link-to-your-image.jpg');
+	      // document.querySelector('meta[name="twitter:image"]').setAttribute('content', 'https://' + hostname + '/link-to-your-image.jpg');
+	    });
