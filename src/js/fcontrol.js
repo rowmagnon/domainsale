9686a978f0254cd9f204160c19c0008966eb943c
magnus <11580453+rowmagnon@users.noreply.github.com>
2024-01-27 17:49:29 +1100

    Update fcontrol.js
    Beautify JS

--- src/js/fcontrol.js
+++ src/js/fcontrol.js
@@ -1,36 +1,41 @@
-	Vue.createApp({
-	  data() {
-	    return {
-	      offer: {
-	        name: '',
-	        email: '',
-	        amount: '',
-	        domainName: window.location.hostname // Gets the domain name from the current URL
-	      }
-	    };
-	  },
-	  methods: {
-	    async submitForm() {
-	      try {
-	        const response = await fetch('https://formspree.io/f/mdoqkyel', {
-	          method: 'POST',
-	          headers: {
-	            'Content-Type': 'application/json'
-	          },
-	          body: JSON.stringify(this.offer)
-	        });
-	
-	        if (response.ok) {
-	          // Handle success - reset form, show message, etc.
-	          console.log('Form submitted successfully');
-	          this.offer = { name: '', email: '', amount: '', domainName: window.location.hostname };
-	        } else {
-	          // Handle error
-	          console.error('Form submission error');
-	        }
-	      } catch (error) {
-	        console.error('There was an error submitting the form:', error);
-	      }
-	    }
-	  }
-	}).mount('#app');
+Vue.createApp({
+	data() {
+		return {
+			offer: {
+				name: '',
+				email: '',
+				amount: '',
+				domainName: window.location.hostname // Gets the domain name from the current URL
+			}
+		};
+	},
+	methods: {
+		async submitForm() {
+			try {
+				const response = await fetch('https://formspree.io/f/mdoqkyel', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(this.offer)
+				});
+
+				if (response.ok) {
+					// Handle success - reset form, show message, etc.
+					console.log('Form submitted successfully');
+					this.offer = {
+						name: '',
+						email: '',
+						amount: '',
+						domainName: window.location.hostname
+					};
+				} else {
+					// Handle error
+					console.error('Form submission error');
+				}
+			} catch (error) {
+				console.error('There was an error submitting the form:', error);
+			}
+		}
+	}
+}).mount('#app');
