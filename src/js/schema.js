b8bdd99503e53fd0321e6591a8af81f3f6157da0
magnus <11580453+rowmagnon@users.noreply.github.com>
2024-01-27 17:51:44 +1100

    Update schema.js
    Beautify JS

--- src/js/schema.js
+++ src/js/schema.js
@@ -1,22 +1,22 @@
-	  document.addEventListener('DOMContentLoaded', function() {
-	    var hostname = window.location.hostname;
-	    var schema = {
-	      "@context": "https://schema.org/",
-	      "@type": "Product",
-	      "name": hostname,
-	      "image": `https://${hostname}/placeholder.jpg`, // Replace with a relevant image URL
-	      "description": `Secure ${hostname} to give your business the online identity it deserves. Simple, memorable, and ready for you to make it your own.`,
-	      "brand": {
-	        "@type": "Brand",
-	        "name": hostname
-	      },
-	      "offers": {
-	        "@type": "Offer",
-	        "url": `https://${hostname}`,
-	        "priceCurrency": "USD",
-	        "price": "7,500" // Replace with the relevant price
-	      }
-	    };
-	
-	    document.getElementById('schemaMarkup').textContent = JSON.stringify(schema);
-	  });
+document.addEventListener('DOMContentLoaded', function () {
+	var hostname = window.location.hostname;
+	var schema = {
+		"@context": "https://schema.org/",
+		"@type": "Product",
+		"name": hostname,
+		"image": `https://${hostname}/placeholder.jpg`, // Replace with a relevant image URL
+		"description": `Secure ${hostname} to give your business the online identity it deserves. Simple, memorable, and ready for you to make it your own.`,
+		"brand": {
+			"@type": "Brand",
+			"name": hostname
+		},
+		"offers": {
+			"@type": "Offer",
+			"url": `https://${hostname}`,
+			"priceCurrency": "USD",
+			"price": "7,500" // Replace with the relevant price
+		}
+	};
+
+	document.getElementById('schemaMarkup').textContent = JSON.stringify(schema);
+});
