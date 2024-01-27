53708e2b3b15706f4f34765b85e9c3b978b3825d
magnus <11580453+rowmagnon@users.noreply.github.com>
2024-01-27 17:48:19 +1100

    Update domainget.js

--- src/js/domainget.js
+++ src/js/domainget.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+	var domainName = window.location.hostname;
 
+	if (domainName.startsWith('www.')) {
+		domainName = domainName.substring(4);
+	}
+	document.getElementById('domain-name').textContent = domainName;
+	document.getElementById('domain-name-input').value = domainName;
+});
